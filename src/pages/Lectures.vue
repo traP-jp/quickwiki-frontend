287e8dbf51b698cd456aab54de1c3b5282f91ae1
{userName} <{userEmail}>
2024-08-12 20:32:00 +0900

    マージしないやつ

--- src/pages/Lectures.vue
+++ src/pages/Lectures.vue
@@ -1,3 +1,36 @@
+<script setup lang="ts">
+
+
+const sougou = 
+
+
+</script>
+
 <template>
-    <h1>Lectures</h1>
+    <h1>Lectures 講習会資料</h1>
+    <div>
+        <button class="side-bar" @click="sougou">総合/融合</button>
+        <button class="side-bar" @click="SysAd">SysAd</button>
+        <button class="side-bar" @click="algorithm">algorithm</button>
+        <button class="side-bar" @click="CTF">CTF</button>
+        <button class="side-bar" @click="Kaggle">Kaggle</button>
+        <button class="side-bar" @click="Game">Game</button>
+        <button class="side-bar" @click="sound">sound</button>
+        <button class="side-bar" @click="graphics">graphics/design</button>
+
+    </div>
 </template>
+
+<style >
+.side-bar {
+  margin: 5px;
+  padding: 10px;
+  background-color: #f0f0f0;
+  border: none;
+  cursor: pointer;
+}
+
+.side-bar:hover {
+  background-color: #ccc;
+}
+</style>
