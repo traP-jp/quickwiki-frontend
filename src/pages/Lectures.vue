b5daf86dc520a3cc7cfce3528104e1a04d4dd047
{userName} <{userEmail}>
2024-08-14 17:16:33 +0900

    Wikiページのデザイン以外

--- src/pages/Lectures.vue
+++ src/pages/Lectures.vue
@@ -18,22 +18,17 @@ const dictonary = {
     'others':'その他講習会',
 }
 
-
-
 onMounted(async () => {
     const res = await fetch('/api/lecture/byFolder/path?folderpath=' + useRoute().params.teams)
     if (res.ok) {
         lectures.value = await res.json()
-    }
+    } 
+    console.log(change.value)
 })
-console.log(dictonary);
-console.log(useRoute().params.teams);
-if (!Array.isArray(useRoute().params.teams)){
-    console.log(dictonary[useRoute().params.teams])
-}
-
-const change = ref<string>('') 
+const change = ref<string>('')
 change.value=dictonary[useRoute().params.teams]
+
+
 
 </script>
 <template>
@@ -41,20 +36,20 @@ change.value=dictonary[useRoute().params.teams]
   <main>
     <div :class="$style.container">
         <header :class="$style.header">
-            
+            {{ change }}
         </header>
 
         <sidebar :class="$style.sidebar">
             <h2>講習会</h2>
-            <router-link to="/lectures/sougou">総合/融合</router-link>
-            <router-link to="/lectures/SysAd">SysAd</router-link>
-            <router-link to="/lectures/algorithm">アルゴリズム</router-link>
-            <router-link to="/lectures/CTF">CTF</router-link>
-            <router-link to="/lectures/Kaggle">Kaggle</router-link>
-            <router-link to="/lectures/Game">ゲーム</router-link>
-            <router-link to="/lectures/sound">サウンド</router-link>
-            <router-link to="/lectures/graphics">グラフィック/デザイン</router-link>
-            <router-link to="/lectures/others"><h2>その他講習会</h2></router-link>
+            <router-link @click="change=dictonary['sougou']" to="/lectures/sougou">総合/融合</router-link>
+            <router-link @click="change=dictonary['SysAd']" to="/lectures/SysAd">SysAd</router-link>
+            <router-link @click="change=dictonary['algorithm']" to="/lectures/algorithm">アルゴリズム</router-link>
+            <router-link @click="change=dictonary['CTF']" to="/lectures/CTF">CTF</router-link>
+            <router-link @click="change=dictonary['Kaggle']" to="/lectures/Kaggle">Kaggle</router-link>
+            <router-link @click="change=dictonary['Game']" to="/lectures/Game">ゲーム</router-link>
+            <router-link @click="change=dictonary['sound']" to="/lectures/sound">サウンド</router-link>
+            <router-link @click="change=dictonary['graphics']" to="/lectures/graphics">グラフィック/デザイン</router-link>
+            <router-link @click="change=dictonary['others']" to="/lectures/others"><h2>その他講習会</h2></router-link>
         </sidebar>
 
     </div>
