48281ea318f26ac87c46110193ae138eadcb2b56
shota <shotashiina132@gmail.com>
2024-08-18 18:50:19 +0900

    匿名sodanのmdEditorにtableの補完機能の追加、個別のSodanページの見た目の修正

--- src/pages/CreateSodan.vue
+++ src/pages/CreateSodan.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue'
+import { compile, ref, watch } from 'vue'
 import { Marked } from 'marked'
 import hljs from 'highlight.js'
 import { markedHighlight } from 'marked-highlight'
 import 'highlight.js/styles/github-dark.css'
 import { log } from 'console'
+import { extractIdentifiers } from 'vue/compiler-sfc'
 
 const title = ref<string>('');
 const SodanContent = ref<string>("");
@@ -24,19 +25,24 @@ const marked = new Marked(markedHighlight({
 );
 marked.setOptions({ breaks: true });
 
+// 2回以上改行した際の補正
 const CheckBeforeRow = (count: number) =>{
     for(let i=count-1; i >= 0; i--){
         if(afterContents.value[i] != "") return i + 1;
     }
     return -1;
 }
-
-const CheckList = (pos: number) => {
+// enterで改行されて増えた行のindexを探す
+const searchChangedRow = () =>{
     afterContents.value = SodanContent.value.split(/\n/);
     let count = -1;
     for(let i=0; i <= beforeContents.value.length; i++){
         if(beforeContents.value[i] != afterContents.value[i]){
-            count = i;
+            if(afterContents.value[i] == ""){
+                count = i;
+            }else{
+                count = i+1;
+            }
             break;
         }
     }
@@ -47,44 +53,150 @@ const CheckList = (pos: number) => {
     // console.log(count);
 
     count = CheckBeforeRow(count)
-    // console.log(count);
-    if(count >= 0){
-        // console.log(afterContents.value)
-        const guessCorsorPos = afterContents.value.slice(0,count).join('\n').length + 1
-        // console.log(afterContents.value.slice(0,count).join('\n'));
-        console.log(guessCorsorPos)
-        if(guessCorsorPos == pos){
-            let countTab = 0;
-            while(countTab + 2 <= afterContents.value[count-1].length){
-                console.log(afterContents.value[count-1].slice(countTab*2,countTab*2+2))
-                if(afterContents.value[count-1].slice(countTab*2,countTab*2+2) == "  "){
-                    countTab++;
-                }else{
-                    break;
-                }
-            }
-            console.log(countTab);
-            console.log(afterContents.value[count-1].slice(countTab,countTab + 2));
-            
-            if(afterContents.value[count-1].slice(countTab*2,countTab*2 + 2) == "- "){
-                console.log(afterContents.value[count-1].slice(countTab*2))
-                if(afterContents.value[count-1].slice(countTab*2) == "- "){
-                    afterContents.value = afterContents.value.slice(0,count-1).concat(afterContents.value.slice(count));
-                    return pos-3-countTab*2
-                    // afterContents.value[count-1] = "";
-                    // console.log(afterContents.value)
-                }else{
-                    let tmp= "";
-                    for(let i=0;i < countTab;i++){
-                        tmp = tmp + "  ";
-                    }
-                    afterContents.value[count] = tmp + "- ";
-                    return pos+2+countTab*2
-                    // console.log(afterContents.value)
-                }
-            }
-        }        
+
+    return count
+}
+
+const guessCorsorPosisCorsorPos = (count: number, pos: number) =>{
+    const guessCorsorPos = afterContents.value.slice(0,count).join('\n').length + 1;
+    return guessCorsorPos == pos
+}
+const CheckList = (count: number, pos: number) => {
+    let countTab = 0;
+    while(countTab + 2 <= afterContents.value[count-1].length){
+        // console.log(afterContents.value[count-1].slice(countTab*2,countTab*2+2))
+        if(afterContents.value[count-1].startsWith("  ",countTab*2)){
+            countTab++;
+        }else{
+            break;
+        }
     }
+    // console.log(countTab);
+    // console.log(afterContents.value[count-1].slice(countTab,countTab + 2));
+    
+    if(afterContents.value[count-1].startsWith("- ",countTab*2)){
+        // console.log(afterContents.value[count-1].slice(countTab*2))
+        if(afterContents.value[count-1].slice(countTab*2) == "- "){
+            afterContents.value = afterContents.value.slice(0,count-1).concat(afterContents.value.slice(count));
+            console.log(afterContents.value)
+            return pos-3-countTab*2
+            // afterContents.value[count-1] = "";
+            
+        }else{
+            let tmp= "";
+            for(let i=0;i < countTab;i++){
+                tmp += "  ";
+            }
+            afterContents.value[count] = tmp + "- " + afterContents.value[count];
+            console.log(afterContents.value)
+            return pos+2+countTab*2
+            
+        }
+    }
+    return pos;
+}   
+const getCellcontents =(row: string) =>{
+    let columns = row.split("|")
+    columns.shift()
+    columns.pop()
+    return columns
+} 
+const getRownum = (example: string) =>{
+    return getCellcontents(example).length
+} 
+const getTable = (count: number, rownum: number) =>{
+    let minRow = count-1;
+    let maxRow = count-1;
+    let target = "";
+    let i = 0;
+    for(i=count-2; i >= 0; i--){
+        if(beforeContents.value[i].startsWith("|") && beforeContents.value[i].endsWith("|") && getRownum(beforeContents.value[i]) == rownum){
+            minRow = i;
+        }else{
+            break;
+        }
+    }
+    for(i=count; i < beforeContents.value.length; i++){
+        if(beforeContents.value[i].startsWith("|") && beforeContents.value[i].endsWith("|") && getRownum(beforeContents.value[i]) == rownum){
+            maxRow = i;
+        }else{
+            break;
+        }
+    }
+
+    let table = [[""]];
+    for(i = 0; i < maxRow-minRow+1; i++){
+        table[i] = getCellcontents(beforeContents.value[i])
+    }
+
+    return {table, maxRow, minRow};
+}
+const CreateRow = (rownum: number, maxlength: number[]) =>{
+    let result = "|"
+    for(let i=0; i < rownum; i++){
+        for(let j=0; j < maxlength[i]; j++){
+            result += " ";
+        }
+        result += "|";
+    }
+    return result;
+}
+const aryMax = (x: number,y: number) =>{
+    return Math.max(x,y);
+}
+const getMaxCellLength = (table: string[][]) =>{
+    let Maxlength: number[] = Array(table.length).fill(0);
+    let length: number[] = Array(table[0].length).fill(0);
+    // console.log(table.length)
+    // console.log(table[0].length)
+    for(let col = 0; col < table[0].length; col++){
+        for(let row = 0; row < table.length; row++){
+            // console.log(length[row])
+            length[row] = table[row][col].length;
+            // console.log(length[row])
+        }
+        // console.log(length)
+        Maxlength[col] = length.reduce(aryMax);
+        // console.log(Maxlength[col]);
+        length = new Array(table[0].length).fill(0);
+    }
+    return Maxlength;
+}
+const setCellLength = (table: string[][], maxLength: number[]) =>{
+    let newtable: string[][] = new Array(table.length).fill(new Array(table[0].length).fill(""));
+    let tmp: string[] = new Array(table[0].length).fill("")
+    for(let row=0; row < table.length; row++){
+        console.log(newtable[row]);
+        tmp = table[row]
+        for(let col=0; col < table[0].length; col++){
+
+            for(let i=0; maxLength[col] > table[row][col].length; i++){
+                
+                // console.log(i,maxLength[col],table[row][col].length, i+1 < maxLength[col] - table[row][col].length)
+                tmp[col] = tmp[col] + " ";
+            }
+        }
+        newtable[row] = tmp
+    }
+    console.log(newtable);
+    
+    return newtable;
+}
+const CheckTable = (count: number, pos: number) =>{
+    console.log(beforeContents.value)
+    const rownum = getRownum(beforeContents.value[count-1])
+    const tables = getTable(count,rownum);
+    console.log(tables);
+    const maxlength = getMaxCellLength(tables.table)
+    console.log(maxlength);
+    const newtable = setCellLength(tables.table,maxlength);
+    console.log(newtable);
+    for(let i=0; i <= tables.maxRow - tables.minRow; i++){
+        afterContents.value[i + tables.minRow] = "|" + newtable[i].join("|") + "|";
+    }
+    
+    afterContents.value[count] = CreateRow(rownum, maxlength);
+
     return pos;
 }
 
@@ -103,13 +215,28 @@ const SubmitSodan = async() =>{
     //     wikis.value = await responce.json();
     // }
 }
+const Check = (pos: number) =>{
+    const count = searchChangedRow();
+    let result = pos; 
+    if(count >= 0){
+        if(guessCorsorPosisCorsorPos(count,pos)){
+            if(beforeContents.value[count-1].startsWith("|") && beforeContents.value[count-1].endsWith("|")){
+                result = CheckTable(count, pos);
+            }else{
+               result =  CheckList(count, pos);
+            }
+            return result;
+        }
+    }
+    return pos
+}
 const toMarkDown = async() =>{
     if(!lockFlg.value){
         // console.log('before:' + document.querySelector('textarea')?.selectionStart)
         let corsorPos = document.querySelector('textarea')?.selectionStart
         if(enterFlg.value && corsorPos){
             lockFlg.value = true;
-            corsorPos = CheckList(corsorPos);
+            corsorPos = Check(corsorPos);
             SodanContent.value = afterContents.value.join('\n');
         }
         if(tabFlg.value && corsorPos){
@@ -161,6 +288,7 @@ watch(SodanContent, toMarkDown);
     <!-- <buton type="button" @click="SubmitSodan"></buton> -->
 </template>
 <style module>
+
 input{
     border:1px solid lightgray;
     width: 90%;
@@ -178,6 +306,7 @@ textarea{
     display: flex;
 }
 .viewer{
+    padding: 10px;
     border:1px solid lightgray;
     border-radius: 10px;
     overflow: scroll;
