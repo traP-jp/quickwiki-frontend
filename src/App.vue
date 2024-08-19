669b6b4a55df1be0cd772af0fa0513d130d40af1
{userName} <{userEmail}>
2024-08-19 18:58:22 +0900

    訂正版

--- src/App.vue
+++ src/App.vue
@@ -6,19 +6,5 @@ import Header from "./components/Header.vue";
   <div>
     <Header />
     <router-view />
-    <div>
-      <footer>
-        <router-link to="/mywiki">QuickWiki</router-link>|
-        <router-link to="/createsodan">CreateSodan</router-link>|
-        <router-link to="/sodan/1">Sodan1</router-link>|
-        <router-link to="/search">Search</router-link>|
-        <router-link to="/lectures/sougou">Lectures</router-link>|
-        <router-link to="/mysodan">MySodan</router-link>|
-        <router-link to="/creatememo">CreateMemo</router-link>|
-        <router-link to="/mywiki">MyWiki</router-link>|
-        <router-link to="/apitest">apitest</router-link>|
-        <router-link to="/tekitounayatu">notfound</router-link>|
-      </footer>
-    </div>
   </div>
 </template>
