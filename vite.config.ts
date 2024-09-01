20d8b279e29ace2d011429a57534ffbfac722ad0
shota <shotashiina132@gmail.com>
2024-09-01 15:25:42 +0900

    proxyけした

--- vite.config.ts
+++ vite.config.ts
@@ -11,14 +11,14 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        rewrite: (path: string) => path.replace(/^\/api/, '/')
-      }
-    }
   }
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:8080',
+  //       changeOrigin: true,
+  //       rewrite: (path: string) => path.replace(/^\/api/, '/')
+  //     }
+  //   }
+  // }
 })
