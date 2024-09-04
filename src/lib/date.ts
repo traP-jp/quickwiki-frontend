5223b9d010871ca819b50062c6c7b8e2b56e976c
kavos113 <83275498+kavos113@users.noreply.github.com>
2024-09-04 15:31:46 +0900

    Update date.ts

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,4 +1,4 @@
 export function convertDate(dateISO: string): string {
     const date = new Date(dateISO);
-    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+    return `${date.getFullYear()}年${date.getMonth() + 1}月${date.getDate()}日`;
 }
