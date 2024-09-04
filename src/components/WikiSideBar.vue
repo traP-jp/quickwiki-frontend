b5581f540922d86b02a9e9d1b6dc8a9382d29d5f
kavos113 <kohei.hadano@gmail.com>
2024-09-04 22:05:40 +0900

    wiki sidebar

--- src/components/WikiSideBar.vue
+++ src/components/WikiSideBar.vue
@@ -3,6 +3,7 @@ import { onBeforeUpdate, onMounted, ref, Ref } from 'vue';
 import { watch } from 'vue';
 import HeaderList from "./HeaderList.vue";
 import HeaderListItems from "./HeaderListItems.vue";
+import { useRoute } from 'vue-router';
 
 const props =defineProps<{
   isMyPage: Boolean
@@ -20,6 +21,9 @@ watch(() => props.isMyPage,
     }
   }
 );
+watch(useRoute(), () => {
+  isHideSidebar.value = true;
+});
 onMounted(() =>{
   window.addEventListener('resize', () => {
     isSmallScreen.value = window.innerWidth <= 960;
@@ -29,34 +33,45 @@ onMounted(() =>{
 const changeSideBarStatus = () => {
   isHideSidebar.value = !isHideSidebar.value;
 }
+const closeWholeClick = (e) => {
+  if(e.target.classList.contains("whole_overlay")){
+    isHideSidebar.value = !isHideSidebar.value;
+  }
+}
+
+const closeSidebar = () => {
+  isHideSidebar.value = true;
+}
 </script>
 <template>
   <button v-if="isSmallScreen" class="sidebar_button" @click="changeSideBarStatus">
-    aaa
-    <font-awesome-icon :icon="['fas', 'bars']" />
+    <span class="sidebar_button_bar sidebar_button_bar_1" :class="{ sidebar_button_bar_hide_1: isHideSidebar }"></span>
+    <span class="sidebar_button_bar sidebar_button_bar_2" :class="{ sidebar_button_bar_hide_2: isHideSidebar }"></span>
+    <span class="sidebar_button_bar sidebar_button_bar_3" :class="{ sidebar_button_bar_hide_3: isHideSidebar }"></span>
   </button>
   <div class="sidebar" :class="{ sidebar_hide: isHideSidebar }">
     <div class="sidebar_content">
       <ul>
         <header-list-items v-if="isSmallScreen" class="sidebar_header_list" />
-        <router-link to="/wiki/mywiki">
+        <router-link to="/wiki/mywiki" @click="closeSidebar">
           <li class="sidebar_link_content">自分のWiki</li>
         </router-link>
-        <a href="#memo" v-if="mypage">
+        <a href="#memo" v-if="mypage" @click="closeSidebar">
           <li class="headerLink sidebar_link_content">備忘録一覧</li>
         </a>
-        <a href="#sodan" v-if="mypage">
+        <a href="#sodan" v-if="mypage" @click="closeSidebar">
           <li class="headerLink sidebar_link_content">相談一覧</li>
         </a>
-        <router-link to="/wiki/favoritewiki">
+        <router-link to="/wiki/favoritewiki" @click="closeSidebar">
           <li class="sidebar_link_content">お気に入りのWiki</li>
         </router-link>
-        <router-link to="/wiki/creatememo">
+        <router-link to="/wiki/creatememo" @click="closeSidebar">
           <li class="sidebar_link_content">Wikiを書く</li>
         </router-link>
       </ul>
     </div>
   </div>
+  <div class="whole_overlay" v-if="isSmallScreen" :class="{ whole_overlay_active: isHideSidebar }" @click="closeWholeClick"></div>
 </template>
 
 <style scoped>
@@ -119,11 +134,26 @@ const changeSideBarStatus = () => {
     font-size: 20px;
     font-weight: bold;
     position: fixed;
-    top: 0;
+    top: 15px;
+    left: 15px;
+    width: 50px;
+    height: 50px;
     z-index: 15;
+    background: none;
+    border-radius: 50%;
+    transition-property: background-color;
+    transition-duration: 0.2s;
+  }
+  .sidebar_button:hover {
+    cursor: pointer;
+    background-color: #f0f0f0;
+  }
+  .sidebar_button:focus,
+  .sidebar_button:focus-visible {
+    outline: none;
   }
   .sidebar_hide {
-    transform: scaleX(0);
+    transform: translateX(-100%);
   }
   .sidebar {
     z-index: 14;
@@ -133,6 +163,59 @@ const changeSideBarStatus = () => {
     height: 110vh;
     transition-property: transform;
     transition-duration: 0.2s;
+  }
+  .sidebar_content {
+    top: 70px;
+  }
+  .sidebar_button_bar {
+    position: absolute;
+    left: 10px;
+    width: 30px;
+    height: 2px;
+    background-color: #000000;
+    border-radius: 2px;
+  }
+  .sidebar_button_bar_1 {
+    top: 15px;
+    transform: translateY(10px) rotate(45deg);
+    transition-property: transform;
+    transition-duration: 0.2s;
+  }
+  .sidebar_button_bar_2 {
+    top: 25px;
+    opacity: 0;
+    transition-property: opacity;
+    transition-duration: 0.2s;
+  }
+  .sidebar_button_bar_3 {
+    top: 35px;
+    transform: translateY(-10px) rotate(-45deg);
+    transition-property: transform;
+    transition-duration: 0.2s;
+  }
+  .sidebar_button_bar_hide_1 {
+    transform: translateY(0) rotate(0);
+  }
+  .sidebar_button_bar_hide_2 {
+    opacity: 1;
+  }
+  .sidebar_button_bar_hide_3 {
+    transform: translateY(0) rotate(0);
+  }
+  .whole_overlay {
+    opacity: 1;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.25);
+    z-index: 13;
+    transition-property: opacity;
+    transition-duration: 0.2s;
+  }
+  .whole_overlay_active {
+    opacity: 0;
   }
 }
 </style>
