a22e45472bc6edbc3ff9500ff61aa7665e53f76a
kavos113 <kohei.hadano@gmail.com>
2024-09-08 03:44:47 +0900

    fix bug

--- src/pages/ApiTest.vue
+++ src/pages/ApiTest.vue
@@ -2,14 +2,33 @@
 import { onMounted, ref } from 'vue'
 
 const pong = ref<string>('no data')
+const user = ref<string>('')
 
 onMounted(async () => {
     const res = await fetch('/api/ping')
     if (res.ok) {
         pong.value = await res.text()
     }
+
+    const resUser = await fetch('/api/me')
+    if (resUser.ok) {
+      const me = await resUser.json()
+      user.value = me.traqId
+    }
 })
+
+const index = () => {
+  if(user.value !== "kavos") {
+    return
+  } else {
+    const res = await fetch('/setting/index')
+    if (res.ok) {
+      pong.value = "OK"
+    }
+  }
+}
 </script>
 <template>
     {{ pong }}
+  <button @click="index">Reset</button>
 </template>
